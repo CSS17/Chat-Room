fdaa84b29e58067a147d31d50d14dac58c4ff2ff
CSS17 <ofucer99@gmail.com>
2023-07-25 09:23:20 +0300

    -ChatViewModel was useless right now. -Gonna be usefull soon

--- app/src/main/java/com/example/chatapp/ChatViewModel.kt
+++ app/src/main/java/com/example/chatapp/ChatViewModel.kt
@@ -9,34 +9,7 @@ import io.socket.emitter.Emitter
 
 class ChatViewModel : ViewModel() {
 
-    private val _chatMessages = MutableLiveData<String>()
-    val chatMessages: LiveData<String>
-        get() = _chatMessages
 
-    private var mSocket: Socket? = null // Nullable Socket türünde değişken
-
-    init {
-        mSocket = SocketHandler.getSocket() // mSocket'ı burada başlatıyoruz
-        observeSocketEvents()
-    }
-
-    private fun observeSocketEvents() {
-        mSocket?.on("counter", onCounterEvent)
-    }
-
-    private val onCounterEvent = Emitter.Listener { args ->
-        if (args.isNotEmpty()) {
-            val message = args[0] as String
-            val name = args[1] as String
-            val currentMessages = _chatMessages.value ?: ""
-            val newMessage = "$name: $message\n"
-            _chatMessages.postValue(currentMessages + newMessage)
-        }
-    }
-
-    fun sendMessage(nickname: String, message: String) {
-        mSocket?.emit("counter", nickname, message)
-    }
 
 }
 
