ab1795f80aad04998e289f94341d2495a912c88a
CSS17 <ofucer99@gmail.com>
2023-07-25 11:04:49 +0300

    -ChatViewModel now can be used with sockets.But the socket issues will move to another class soon. -ChatRoom activity is more simple now.

--- app/src/main/java/com/example/chatapp/ChatRoom.kt
+++ app/src/main/java/com/example/chatapp/ChatRoom.kt
@@ -3,57 +3,43 @@ package com.example.chatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.chatapp.SocketHandler.closeConnection
 import kotlinx.android.synthetic.main.activity_chat_room.*
 
 class ChatRoom : AppCompatActivity() {
-    private var text: String = ""
-    private var onlineUser: Int = 0 // Online kullanıcı sayısını takip edeceğimiz değişken
+    private var message: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
-        val nickname = intent.getStringExtra("nickname")
+        val nickname = intent.getStringExtra("nickname").toString()
 
-        SocketHandler.setSocket()
-        val mSocket = SocketHandler.getSocket()
-        mSocket.connect()
+        val viewModel = ViewModelProvider(this).get(ChatViewModel::class.java)
 
         // Online kullanıcı sayısını takip etmek için olay dinleyicisini başlatıyoruz
-        mSocket.on("onlineUser") { args ->
-            if (args.isNotEmpty()) {
-                onlineUser = args[0] as Int
-                runOnUiThread {
-                    online_user_count.text = "Online Users: $onlineUser"
-                }
+        viewModel.showOnlineUser { onlineUser ->
+            runOnUiThread {
+                online_user_count.text = "Online Users: $onlineUser"
             }
         }
 
         send_btn.setOnClickListener {
             Log.d("TAG", editText.text.toString())
-            text = editText.text.toString()
+            message = editText.text.toString()
             editText.setText("")
-            mSocket.emit("counter", nickname, text)
+            viewModel.sendMessage(nickname, message)
             scrollChatToBottom()
         }
 
-        mSocket.on("counter") { args, ->
-            Log.d("TAG", args.size.toString())
-            if (args.isNotEmpty()) {
-                Log.d("TAG", args[0].toString())
-                val message = args[0] as String
-                val name = args[1] as String
-                runOnUiThread {
-                    val currentText = chat.text.toString()
-                    val newText = "$name: $message\n"
-                    chat.text = currentText + newText
-                    scrollChatToBottom()
-                }
+        viewModel.showReceivedMessage { name, message ->
+            runOnUiThread {
+                val currentText = chat.text.toString()
+                val newText = "$name: $message\n"
+                chat.text = currentText + newText
+                scrollChatToBottom()
             }
-
         }
-
-
     }
 
     private fun scrollChatToBottom() {
@@ -67,12 +53,10 @@ class ChatRoom : AppCompatActivity() {
         }
     }
 
-
-
     override fun onDestroy() {
         super.onDestroy()
         closeConnection()
     }
-
-
 }
+
+
--- app/src/main/java/com/example/chatapp/ChatViewModel.kt
+++ app/src/main/java/com/example/chatapp/ChatViewModel.kt
@@ -1,17 +1,45 @@
 package com.example.chatapp
 
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.view.textclassifier.ConversationActions.Message
 import androidx.lifecycle.ViewModel
 import io.socket.client.Socket
-import io.socket.emitter.Emitter
+
 
 class ChatViewModel : ViewModel() {
+    private var onlineUser: Int = 0
+    var mSocket: Socket
 
+    init {
+        SocketHandler.setSocket()
+        mSocket = SocketHandler.getSocket()
+        mSocket.connect()
+    }
 
+    fun showOnlineUser(callback: (Int) -> Unit) {
+        mSocket.on("onlineUser") { args ->
+            if (args.isNotEmpty()) {
+                onlineUser = args[0] as Int
+                callback(onlineUser)
+            }
+        }
+    }
 
+    fun showReceivedMessage(callback: (String, String) -> Unit) {
+        mSocket.on("counter") { args ->
+            if (args.isNotEmpty()) {
+                val message = args[0] as String
+                val name = args[1] as String
+                callback(name, message)
+            }
+        }
+    }
+
+    fun sendMessage(nickname:String,message:String){
+        mSocket.emit("counter",nickname,message)
+    }
 }
+
+
 
 
 
